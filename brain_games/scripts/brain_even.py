9b9eb2f675b725206daa04a5ca60caf67a7b9abe
ILya Gurev <ilya.gurev.94@mail.ru>
2022-08-17 20:15:04 +0500

    add new file brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.games.engine import start_games
+from brain_games.games import even
+
+
+def main():
+    start_games.start(even)
+
+
+if __name__ == '__main__':
+    main()
