b55c6413b3de1fc95efd8c7bb8f18c7ad204b977
ILya Gurev <ilya.gurev.94@mail.ru>
2022-08-17 20:12:32 +0500

    add new file progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+TEXT_GAME = 'What number is missing in the progression?'
+
+
+def question_and_answer():
+    progression = list(range(randint(1, 10), randint(35, 50), randint(2, 5)))
+    progression = progression[:10]
+    random_index = randint(0, len(progression) - 1)
+    correct_answer = progression[random_index]
+    progression[random_index] = '..'
+    progression = list(map(str, progression))
+    progression = ' '.join(progression)
+    correct_answer = str(correct_answer)
+    return progression, correct_answer
