f67916f54e13ce851320cb98a5de4242d520430e
ILya Gurev <ilya.gurev.94@mail.ru>
2022-08-17 20:10:57 +0500

    add new file even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+TEXT_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
+RANDOM_MIN_VALUE = 1
+RANDOM_MAX_VALUE = 100
+
+
+def question_and_answer():
+    random_number = randint(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE)
+    if random_number % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return random_number, correct_answer
