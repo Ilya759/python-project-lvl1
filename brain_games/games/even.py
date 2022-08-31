9d5cdbfcafcab7278d1ea166e2d94f340c090cb4
ILya Gurev <ilya.gurev.94@mail.ru>
2022-09-01 02:36:07 +0500

    upgrade file even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,9 +6,15 @@ RANDOM_MIN_VALUE = 1
 RANDOM_MAX_VALUE = 100
 
 
+def is_even(random_number):
+    if random_number % 2 == 0:
+        return True
+    return False
+
+
 def generate_data():
     random_number = randint(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE)
-    if random_number % 2 == 0:
+    if is_even(random_number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
