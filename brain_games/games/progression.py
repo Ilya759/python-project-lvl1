0f6b9f7a8999e1606bf3358cd48798e93d2c58e8
ILya Gurev <ilya.gurev.94@mail.ru>
2022-09-01 02:36:42 +0500

    upgrade file progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,16 +11,21 @@ MAXIMUM_PROG_LENGTH = 50
 PROGRESSION_LENGTH = 10
 
 
-def generate_data():
+def get_progression():
     prog_start = randint(MINIMUM_PROG_START, MAXIMUM_PROG_START)
     prog_step = randint(MINIMUM_PROG_STEP, MAXIMUM_PROG_STEP)
     prog_length = randint(MINIMUM_PROG_LENGTH, MAXIMUM_PROG_LENGTH)
     progression = list(range(prog_start, prog_length, prog_step))
+    return progression
+
+
+def generate_data():
+    progression = get_progression()
     progression = progression[:PROGRESSION_LENGTH]
     random_index = randint(0, len(progression) - 1)
     correct_answer = progression[random_index]
     progression[random_index] = '..'
     progression = list(map(str, progression))
-    progression = ' '.join(progression)
+    question = ' '.join(progression)
     correct_answer = str(correct_answer)
-    return progression, correct_answer
+    return question, correct_answer
